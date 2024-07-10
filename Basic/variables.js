4d3c374fc97f513a325cdeb92345981a5df3c03e
Edison Osorio Perez <eosorio@migrante.com>
2024-07-10 14:52:27 -0500

    clase 01 declaración de variables en javascripts

---
+++ Basic/variables.js
@@ -0,0 +1,14 @@
+// HOSTING: es cuando se puede aceder a una variable antes de su unicialización
+// var: es la forma iriginal de declarar variables en javascript, es la namera en la que nose debe declarar variables
+var helloWorld = "¡Hello, JavaScript!";
+console.log(helloWorld);
+
+// let: estas variables solo son acesibles dentro de un bloque
+
+let helloWorld2 = "¡Hello, JavaScript 2!";
+console.log(helloWorld2);
+
+// const: esta permite qu eno se pueda reasignar el valor de una variable ya que estee es constante
+
+const helloWorld3 = "¡Hello, JavaScript 3!";
+console.log(helloWorld3);
